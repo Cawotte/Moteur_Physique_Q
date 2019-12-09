c16a2455cc80b0a5ace4c2fdc805bbf3246d9a71
Ensuque <khamaz.ee@gmail.com>
2019-12-09 13:52:20 -0500

    collision partially working!

--- Moteur_physique/Primitive.cpp
+++ Moteur_physique/Primitive.cpp
@@ -15,13 +15,25 @@ Vector3D Primitive::pointRotationByQuaternion(Vector3D point, Quaternion q)
 
 bool Primitive::pointIsWithinBox(Vector3D point, Vector3D* p)
 {
-	Vector3D u = p[0] - p[1];
-	Vector3D v = p[0] - p[3];
-	Vector3D w = p[0] - p[4];
+	float xLenght = (p[1] - p[0]).norm();
+	float yLenght = (p[3] - p[0]).norm();
+	float zLenght = (p[4] - p[0]).norm();
 
-	return (u.x > u.dotProd(p[0]) && u.x < u.dotProd(p[1]) &&
-		v.x > v.dotProd(p[0]) && v.x < v.dotProd(p[3]) &&
-		w.x > w.dotProd(p[0]) && w.x < w.dotProd(p[4])
+	Vector3D xLocal = (p[1] - p[0]) * (1.f / xLenght);
+	Vector3D yLocal = (p[3] - p[0]) * (1.f / yLenght);
+	Vector3D zLocal = (p[4] - p[0]) * (1.f / zLenght);
+
+	Vector3D pivot = p[8];
+
+	Vector3D v = point - pivot;
+
+	float px = abs(v.dotProd(xLocal));
+	float py = abs(v.dotProd(yLocal));
+	float pz = abs(v.dotProd(zLocal));
+
+	return (2 * px <= xLenght &&
+			2 * py <= yLenght &&
+			2 * pz <= zLenght
 		);
 	
 }
@@ -41,9 +53,7 @@ Vector3D* Primitive::rotatedRepere()
 	points[7] = Vector3D{ bounds_.xmin_, bounds_.ymax_, bounds_.zmax_ }; //p8
 	points[5] = Vector3D{ bounds_.xmax_, bounds_.ymin_, bounds_.zmax_ }; //p6
 
-	points[8] = Vector3D{ (bounds_.xmin_ + bounds_.xmax_) /2.f ,
-						(bounds_.ymin_ + bounds_.ymax_) / 2.f , 
-						 (bounds_.zmin_ + bounds_.zmax_) / 2.f , }; 
+	points[8] = (points[0] + points[7]) * 0.5f;
 
 
 	Quaternion q = body_->getOrientation();
